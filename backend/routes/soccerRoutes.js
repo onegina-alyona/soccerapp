dedd1db283c7d4c1678d7d25afe14ad3134a4da5
onegin-alyona <onegina.alyona@gmail.com>
2020-05-11 19:08:12 +1000

    2020-05-11

---
+++ backend/routes/soccerRoutes.js
@@ -0,0 +1,19 @@
+import { 
+    addNewPlayer, 
+    getPlayers,
+    getPlayerWithID,
+    UpdatePlayer,
+    deletePlayer
+} from '../controllers/playerControllers.js';
+
+const routes = app => {
+    app.route('/players')
+        .get(getPlayers)
+        .post(addNewPlayer);
+    app.route('/player/:PlayerId')
+        .get(getPlayers)
+        .put(UpdatePlayer)
+        .delete(deletePlayer);
+};
+
+export default routes;
